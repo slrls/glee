42e01e74b93c4e140ef1f85461763ee978f382a6
Scott Rogers <srogers@amplify.com>
2014-03-23 15:24:44 -0400

    Remove Safari and outdated stuff

--- outdated/FF/content/common.js
+++
@@ -1,9 +0,0 @@
-var gleebox_Common = {
-	
-	openInNewTab: function(url){
-		var win = Components.classes['@mozilla.org/appshell/window-mediator;1']
-		      .getService(Components.interfaces.nsIWindowMediator)
-		      .getMostRecentWindow('navigator:browser');
-	    win.openUILinkIn(url, 'tab');
-	}
-};
